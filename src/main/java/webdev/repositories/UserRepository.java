4e5d17f2c775577ab5c17fdd280d623064d07ba0
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-22 16:31:02 -0700

    Created UserRepository.java

---
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+/**
+ * This file contains UserRepository implements mapping between Java classes 
+ * and instances and their corresponding relational schemas, tables, and records.
+ */
+package webdev.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import webdev.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
+	Iterable<User> findUserByCredentials(
+		@Param("username") String username, 
+		@Param("password") String password);
+}
+
