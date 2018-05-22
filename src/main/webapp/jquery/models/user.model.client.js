e5c268bf31a54e697254b247f8f314243a4b5fdf
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-21 20:28:11 -0700

    Created user model

---
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -0,0 +1,90 @@
+/**
+ * This file implements a User JavaScript class with the following properties:
+ * username, password, email, firstName, lastName, phone, role, and dateOfBirth.
+ * Use this class to create instances of users whenever sending or receiving
+ * data from the server, or rendering users in the UI.
+ */
+function User(username, password, firstName, lastName, role, email, phone, dateOfBirth) {
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.role = role;
+	this.email = email;
+	this.phone = phone;
+	this.dateOfBirth = dateOfBirth;
+
+	this.setUsername = setUsername;
+	this.getUsername = getUsername;
+	this.setPassword = setPassword;
+	this.getPassword = getPassword;
+	this.setFirstName = setFirstName;
+	this.getFirstName = getFirstName;
+	this.setLastName = setLastName;
+	this.getLastName = getLastName;
+	this.setRole = setRole;
+	this.getRole = getRole;
+	this.setEmail = setEmail;
+	this.getEmail = getEmail;
+	this.setPhone = setPhone;
+	this.getPhone = getPhone;
+	this.setDateOfBirth = setDateOfBirth;
+	this.getDateOfBirth = getDateOfBirth;
+
+	function setUsername(username) {
+		this.username = username;
+	}
+	function getUsername() {
+		return this.username;
+	}
+	
+	function setPassword(password) {
+		this.password = password;
+	}
+	function getPassword() {
+		return this.password;
+	}
+
+	function setFirstName(firstname) {
+		this.firstname = firstname;
+	}
+	function getFirstName() {
+		return this.firstname;
+	}
+	
+	function setLastName(lastname) {
+		this.lastname = lastname;
+	}
+	function getLastName() {
+		return this.lastname;
+	}
+	
+	function setRole(role) {
+		this.role = role;
+	}
+	function getRole() {
+		return this.role;
+	}
+	
+	function setEmail(email) {
+		this.email = email;
+	}
+	function getEmail() {
+		return this.email;
+	}
+	
+	function setPhone(phone) {
+		this.phone = phone;
+	}
+	function getPhone() {
+		return this.phone;
+	}
+	
+	function setDateOfBirth(dateOfBirth) {
+		this.dateOfBirth = dateOfbirth;
+	}
+	function getDateOfBirth() {
+		return this.dateOfBirth;
+	}
+
+}
