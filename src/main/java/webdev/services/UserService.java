4f26fe20abcb4f901f25fd280bd6c817a6e62e6b
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-22 16:56:38 -0700

    Created UserServie.java

---
+++ src/main/java/webdev/services/UserService.java
@@ -0,0 +1,68 @@
+/**
+ * This file contains UserService use repositories to implement their respective behavior,
+ * The Web service endpoints expose the URL patterns and actions needed to support the user
+ * admin client application
+ */
+package webdev.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.repositories.UserRepository;
+import webdev.models.User;
+
+@RestController
+public class UserService {
+	@Autowired
+	UserRepository repository;
+
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repository.deleteById(id);
+	}
+
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
+	}
+
+	@PostMapping("/api/login")
+	public List<User> login(@RequestBody User user) {
+		return (List<User>) repository.findUserByCredentials(user.getUsername(), user.getPassword());
+	}
+
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>) repository.findAll();
+	}
+
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		Optional<User> data = repository.findById(userId);
+		if (data.isPresent()) {
+			User user = data.get();
+			user.setFirstName(newUser.getFirstName());
+			repository.save(user);
+			return user;
+		}
+		return null;
+	}
+
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") int userId) {
+		Optional<User> data = repository.findById(userId);
+		if (data.isPresent()) {
+			return data.get();
+		}
+		return null;
+	}
+}
