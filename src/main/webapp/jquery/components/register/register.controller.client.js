10bccb274b138e83f023a35d0a416e9e57e5b88f
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-23 02:54:57 -0700

    Created register.controller.client.js

---
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -0,0 +1,13 @@
+/**
+ * This file contains the register controller handling user events and rendering
+ * dynamic portions of the user admin page
+ */
+(function () {
+    var $usernameFld, $passwordFld, $verifyPasswordFld;
+    var $registerBtn;
+    var userService = new UserService();
+    $(main);
+
+    // function main() { … }
+    // function register() { … }
+})();
