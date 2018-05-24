acf1e858eff4b98486e60b0d26ace93ee4c11a47
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-23 23:41:47 -0700

    created login.controller.client.js

---
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -0,0 +1,12 @@
+/**
+ *  This file implements the controller handling user events and rendering dynamic portions of the user admin page
+ */
+(function () {
+    var $usernameFld, $passwordFld;
+    var $loginBtn;
+    var userService = new AdminUserServiceClient();
+    $(main);
+
+    // function main() { … }
+    // function login() { … }
+})();
