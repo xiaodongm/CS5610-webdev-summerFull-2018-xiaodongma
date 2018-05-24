ce34a28f074d754eaa051c2ad919826f8916bbc0
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-23 23:49:56 -0700

    added findUserByCredentials in UserRepository.java

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -11,10 +11,10 @@ import org.springframework.data.repository.query.Param;
 import webdev.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-//	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
-//	Iterable<User> findUserByCredentials(
-//		@Param("username") String username, 
-//		@Param("password") String password);
+	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
+	Iterable<User> findUserByCredentials(
+		@Param("username") String username, 
+		@Param("password") String password);
 	
 	@Query("SELECT u FROM User u WHERE u.username=:username")
 	Iterable<User> findUserByUsername(@Param("username") String u);
