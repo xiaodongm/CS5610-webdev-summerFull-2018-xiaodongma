82654a5df8c198c9a90c30c9cce9a80c49961abc
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-24 12:51:23 -0700

    created profile.controller.client.js

---
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -0,0 +1,14 @@
+/**
+ * This file implements the controller handling user events and rendering dynamic portions of the user admin page
+ */
+
+(function () {
+    var $usernameFld, $passwordFld;
+    var $updateBtn, $logoutBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    // function main() { … }
+    // function updateProfile() { … }
+    // function logout() { … }
+})();
