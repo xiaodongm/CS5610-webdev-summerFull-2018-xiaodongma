fb0e44bf423f00b77b1c4976fcc2cb7f92c49b1c
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-31 16:15:24 -0700

    Created course.sql

--- src/main/java/webdev/models/Course.java
+++ src/main/java/webdev/models/Course.java
@@ -1,12 +1,55 @@
 /**
- * 
+ * This file contains Course data model
  */
 package webdev.models;
 
-/**
- * @author Ukelele
- *
- */
-public class Course {
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.*;
+
+
+@Entity
+public class Course {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date modified;
+//	@OneToMany(mappedBy="course")
+//	private List<Module> modules;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getModified() {
+		return modified;
+	}
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+//	public List<Module> getModules() {
+//		return modules;
+//	}
+//	public void setModules(List<Module> modules) {
+//		this.modules = modules;
+//	}
 }
+
--- src/main/java/webdev/services/CourseServices.java
+++ src/main/java/webdev/services/CourseServices.java
@@ -1,12 +1,25 @@
 /**
- * 
+ * This file contains course web services endpoint
  */
 package webdev.services;
 
-/**
- * @author Ukelele
- *
- */
-public class CourseServices {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Course;
+import webdev.repositories.CourseRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CourseServices {
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@GetMapping("/api/course")
+	public Iterable<Course> findAllCourses() {
+		return courseRepository.findAll(); 
+	}
 }
