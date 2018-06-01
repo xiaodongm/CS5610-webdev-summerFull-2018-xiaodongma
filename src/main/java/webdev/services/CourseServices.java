74912df8cf0e53414f60709d7ba3eee43f87e724
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-31 18:31:07 -0700

    implemented deleteCourse in CourseServices.java

--- src/main/java/webdev/services/CourseServices.java
+++ src/main/java/webdev/services/CourseServices.java
@@ -6,7 +6,9 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +32,9 @@ public class CourseServices {
 			return courseRepository.save(course);
 	}
 
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteCourse(@PathVariable("courseId") int id) {
+		courseRepository.deleteById(id);
+	}
 	
 }
