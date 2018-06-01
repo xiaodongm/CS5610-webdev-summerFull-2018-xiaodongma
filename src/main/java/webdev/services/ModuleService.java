49523f5bdc1a3b2119510154b193f2b979a7506a
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-01 16:32:48 -0700

    implemented findAllModulesForCourse() in ModuleService.java

--- src/main/java/webdev/services/ModuleService.java
+++ src/main/java/webdev/services/ModuleService.java
@@ -1,9 +1,11 @@
 package webdev.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +38,16 @@ public class ModuleService {
 		}
 		return null;		
 	}
+	
+	@GetMapping("/api/course/{courseId}/module")
+	public List<Module> findAllModulesForCourse(@PathVariable("courseId") int courseId) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			return course.getModules();
+		}
+		return null;		
+	}
+
 	
 }
