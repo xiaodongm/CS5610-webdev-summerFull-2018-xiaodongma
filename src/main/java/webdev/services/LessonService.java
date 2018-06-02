05ce11bed7ded4bab024a960459f04a9676b399d
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-02 16:39:18 -0700

    Created LessonService.java, implemented CreateLesson, deleteLesson, findAllLessons, findAllLessonsForModule

---
+++ src/main/java/webdev/services/LessonService.java
@@ -0,0 +1,72 @@
+package webdev.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Lesson;
+import webdev.models.Module;
+import webdev.repositories.CourseRepository;
+import webdev.repositories.LessonRepository;
+import webdev.repositories.ModuleRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class LessonService {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	ModuleRepository moduleRepository;
+
+	@Autowired
+	LessonRepository lessonRepository;
+
+	@PostMapping("/api/course/{cid}/module/{mid}/lesson")
+	public Lesson createLesson(
+			@PathVariable("mid") int moduleId,
+			@RequestBody Lesson newLesson) {
+		
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		
+		if (data.isPresent()) {
+			Module module = data.get();
+			newLesson.setModule(module);;
+			return lessonRepository.save(newLesson);
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/api/lesson/{lid}")
+	public void deleteLesson(@PathVariable("lid") int lessonId) {
+		lessonRepository.deleteById(lessonId);
+	}
+	
+	@GetMapping("/api/lesson")
+	public Iterable<Lesson> findAllLessons() {
+		return lessonRepository.findAll(); 
+	}
+
+	@GetMapping("/api/course/{cid}/module/{mid}/lesson")
+	public List<Lesson> findAllLessonsForModule(@PathVariable("mid") int moduleId) {
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		if(data.isPresent()) {
+			Module module = data.get();
+			return module.getLessons();
+		}
+		return null;		
+	}
+	
+	
+	
+
+}
