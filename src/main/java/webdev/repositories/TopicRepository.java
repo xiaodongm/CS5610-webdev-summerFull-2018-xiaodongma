7dcd8ef60a0b7f1433be7aa3ea7b057c5759bb25
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-03 18:11:50 -0700

    Created TopicRepository.java

---
+++ src/main/java/webdev/repositories/TopicRepository.java
@@ -0,0 +1,7 @@
+package webdev.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.Topic;
+
+public interface TopicRepository extends CrudRepository<Topic, Integer>{}
