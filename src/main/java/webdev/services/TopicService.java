d85b0ec65ae4ccbc01bd473bee6784be04d1d688
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-03 18:31:19 -0700

    Created TopicService.java

---
+++ src/main/java/webdev/services/TopicService.java
@@ -0,0 +1,66 @@
+package webdev.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Lesson;
+import webdev.models.Topic;
+import webdev.repositories.LessonRepository;
+import webdev.repositories.TopicRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class TopicService {
+
+	@Autowired
+	LessonRepository lessonRepository;
+	
+	@Autowired
+	TopicRepository topicRepository;
+	
+	@PostMapping("/api/course/{cid}/module/{mid}/lesson/{lid}/topic")
+	public Topic createTopic(
+			@PathVariable("lid") int lessonId,
+			@RequestBody Topic newTopic) {
+		
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		
+		if (data.isPresent()) {
+			Lesson lesson = data.get();
+			newTopic.setLesson(lesson);;
+			return topicRepository.save(newTopic);
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/api/topic/{tid}")
+	public void deleteTopic(@PathVariable("tid") int topicId) {
+		topicRepository.deleteById(topicId);
+	}
+	
+	@GetMapping("/api/topic")
+	public Iterable<Topic> findAllTopics() {
+		return topicRepository.findAll(); 
+	}
+
+	@GetMapping("/api/course/{cid}/module/{mid}/lesson/{lid}/topic")
+	public List<Topic> findAllTopicsForLesson(@PathVariable("lid") int lessonId) {
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		if(data.isPresent()) {
+			Lesson lesson = data.get();
+			return lesson.getTopics();
+		}
+		return null;		
+	}
+	
+	
+}
