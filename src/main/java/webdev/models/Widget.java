2999af7079c55eacec772bbbe707a5f78f102ccf
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-14 15:53:25 -0700

    Added attributes to Widget Model

--- src/main/java/webdev/models/Widget.java
+++ src/main/java/webdev/models/Widget.java
@@ -14,46 +14,151 @@ import javax.persistence.InheritanceType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Widget {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	private String name;
 	private String text;
+	private String href;
+	private String style;
+	private String width;
+	private String height;
+	private int order;
+	private String className;
+	private String src;
+
+	enum ListType {
+		ordered, unordered
+	};
+
+	private ListType listType;
+	private String listItems;
 	private String widgetType;
 	private int size;
 	@ManyToOne
 	@JsonIgnore
 	private Topic topic;
+
 	public String getWidgetType() {
 		return widgetType;
 	}
+
 	public void setWidgetType(String widgetType) {
 		this.widgetType = widgetType;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 	public Topic getTopic() {
 		return topic;
 	}
+
 	public void setTopic(Topic topic) {
 		this.topic = topic;
 	}
+
 	public int getSize() {
 		return size;
 	}
+
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+	public String getWidth() {
+		return width;
+	}
+
+	public void setWidth(String width) {
+		this.width = width;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	public ListType getListType() {
+		return listType;
+	}
+
+	public void setListType(ListType listType) {
+		this.listType = listType;
+	}
+
+	public String getListItems() {
+		return listItems;
+	}
+
+	public void setListItems(String listItems) {
+		this.listItems = listItems;
+	}
+
 }
