b4cb296e6bff251ac2cfdf4eb5b2129e18513c80
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-15 19:53:25 -0700

    added function findWidgetById, deleteWidget and updateWidget

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -1,17 +1,22 @@
 package webdev.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import webdev.models.Course;
 import webdev.models.Topic;
+import webdev.models.User;
 import webdev.models.Widget;
 import webdev.repositories.TopicRepository;
 import webdev.repositories.WidgetRepository;
@@ -58,6 +63,48 @@ public class WidgetService {
 				widgetRepository.save(widget);
 			}
 		}
+	}
+	
+	@GetMapping("/api/widget/{wid}")
+	public Widget findWidgetById(@PathVariable("wid") int id) {
+		Optional<Widget> data = widgetRepository.findById(id);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/api/widget/{wid}")
+	public void deleteWidget(@PathVariable("wid") int widgetId) {
+		Optional<Widget> data = widgetRepository.findById(widgetId);
+		if(data.isPresent()) {
+			data.get().getTopic().getLesson().getModule().getCourse().setModified(new Date());
+		}
+		widgetRepository.deleteById(widgetId);
+	}
+	
+	@PutMapping("/api/widget/{widgetId}")
+	public Widget updateWidget(@PathVariable("widgetId") int widgetId, @RequestBody Widget newWidget) {
+		Optional<Widget> data = widgetRepository.findById(widgetId);
+		if (data.isPresent()) {
+			Widget widget = data.get();
+			widget.setName(newWidget.getName());
+			widget.setId(newWidget.getId());
+			widget.setText(newWidget.getText());
+			widget.setHref(newWidget.getHref());
+			widget.setStyle(newWidget.getStyle());
+			widget.setWidth(newWidget.getWidth());
+			widget.setHeight(newWidget.getHeight());
+			widget.setWidgetOrder(newWidget.getWidgetOrder());
+			widget.setClassName(newWidget.getClassName());
+			widget.setSrc(newWidget.getSrc());
+			widget.setListType(newWidget.getListType());
+			widget.setListItems(newWidget.getListItems());
+			widget.setWidgetType(newWidget.getWidgetType());
+			widget.setSize(newWidget.getId());
+			return widget;
+		}
+		return null;
 	}
 	
 	
