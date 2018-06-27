091a28c1f469cfa92a995d3dc38ec263b1296dac
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-27 15:52:14 -0700

    Created Assignment.java data model

---
+++ src/main/java/webdev/models/Assignment.java
@@ -0,0 +1,35 @@
+/**
+ * This file contains Assignment data model with extends class Widget.
+ */
+package webdev.models;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Assignment extends Widget {
+	private String title;
+	private int points;
+	private String description;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
