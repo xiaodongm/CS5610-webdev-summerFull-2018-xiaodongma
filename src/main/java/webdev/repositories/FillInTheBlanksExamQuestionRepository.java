40f61972ed622fa2fd363f42acbe0b69ae45df11
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-28 16:48:25 -0700

    implemented repositories for Exam, BaseExamQuestion, EssayExamQuestion, FillInTheBlanksQuestion, MultipleChoiceExamQuestion, TrueOrFalseExamQuestion.

---
+++ src/main/java/webdev/repositories/FillInTheBlanksExamQuestionRepository.java
@@ -0,0 +1,9 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import webdev.models.FillInTheBlanksExamQuestion;
+
+public interface FillInTheBlanksExamQuestionRepository extends CrudRepository<FillInTheBlanksExamQuestion, Integer> {
+
+}
